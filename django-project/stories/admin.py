dcb37edd46cdf7734816de4df11f660d90cc8555
Nathan Lawrence <lawrencen@missouri.edu>
2017-09-08 13:08:29 -0500

    Some more helpful UI improvements.

--- django-project/stories/admin.py
+++ django-project/stories/admin.py
@@ -47,15 +47,77 @@ class StoryAdmin(ReverseModelAdmin):
              )
          })
     )
+    filter_horizontal = ['people', 'issues']
     inline_type = 'stacked'
     inline_reverse = ['transcript']
+    prepopulated_fields = {'slug': ('title',)}
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
     list_display = ['name', 'title']
     search_fields = ['name', 'title']
+    fieldsets = (
+        (None, {
+            'fields': (
+                'name',
+                'slug',
+                ('first_name', 'second_name'),
+                'title',
+                'description',
+                'photo'
+            )
+        }),
+    )
+    prepopulated_fields = {'slug': ('name',)}
 
-admin.site.register(Region)
-admin.site.register(Issue)
-admin.site.register(Location)
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'center_lat', 'center_long']
+    fieldsets = (
+        (None, {
+            'fields': (
+                'name',
+                'slug',
+                ('center_lat', 'center_long'),
+                'map_zoom',
+                'description'
+            )
+        }),
+        ('Media', {
+            'fields': (
+                'image',
+                'video_embed_url'
+            )
+        }),
+    )
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'lat', 'long']
+    fieldsets = (
+        (None, {
+            'fields': (
+                'name',
+                'slug',
+                ('lat', 'long'),
+                'description',
+            )
+        }),
+    )
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Issue)
+class IssueAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': (
+                'name',
+                'slug',
+                'description'
+            )
+        }),
+    )
+    prepopulated_fields = {'slug': ('name',)}
+
 admin.site.register(Transcript, TranscriptAdmin)
